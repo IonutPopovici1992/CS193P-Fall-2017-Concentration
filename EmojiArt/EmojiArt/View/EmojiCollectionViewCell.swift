4b6109813f207e43dc3507642c83fdea3a8559c3
John <ionut_popovici92@yahoo.com>
2018-03-21 16:58:26 +0200

    12. Drag and Drop, Table View, Collection View and Text Field

---
+++ EmojiArt/EmojiArt/View/EmojiCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by John on 3/20/18.
+//  Copyright © 2018 John. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
